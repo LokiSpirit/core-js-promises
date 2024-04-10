99e5be1ef8af2baa9e6f257fd9bb53b972c0fb8c
Olga Krukova <krukovaolga1@gmail.com>
2024-04-10 16:45:09 +0300

    feat: pass 2 tests

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,11 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((resolve, reject) => {
+    if (number > 0) resolve(number);
+    else reject(number);
+  });
 }
 
 /**
@@ -33,10 +36,17 @@ function getPromise(/* number */) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return new Promise((resolve) => {
+    source
+      .then(() => {
+        resolve('success');
+      })
+      .catch(() => {
+        resolve('fail');
+      });
+  });
 }
-
 /**
  * Takes an array of promises and returns a promise that resolves with the value of the first successfully resolved promise From the given array.
  * If all promises in the array are rejected, it returns a promise that is rejected.
@@ -50,8 +60,14 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.race(promises).then((result) => {
+    if (result instanceof Error) {
+      throw new Error();
+    } else {
+      return result;
+    }
+  });
 }
 
 /**
